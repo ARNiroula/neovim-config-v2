5e3e0e34355088dbf00265df2dd1ffdf9adf56c1
Anup Raj Niroula <a.r.niroula@gmail.com>
2026-03-07 11:49:02 -0500

    Add Bash LSP to nvim initializer

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- require("nvim-treesitter.install").compilers = { "gcc", "clang" }  -- Deprecated method
 
 -- Modify The Python-LSP-Server Schema
 require("lspconfig").pylsp.setup({
@@ -8,10 +7,10 @@ require("lspconfig").pylsp.setup({
         pylsp = {
             plugins = {
                 pycodestyle = {
-                    maxLineLength = 120,
+                    maxLineLength = 150,
                 },
                 flake8 = {
-                    maxLineLength = 120,
+                    maxLineLength = 150,
                 },
             },
         },
@@ -22,3 +21,5 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.lsp.enable("bashls") -- Enable Bash Lsp
